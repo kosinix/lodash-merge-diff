7b18250a94a52663eb700bc2fb3e1c98094c070e
kosinix <kosinix@users.noreply.github.com>
2019-10-03 14:28:27 +0800

    Add support for Date object

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const lodashSet = require('lodash.set')
 const lodashEach = require('lodash.foreach')
 const differ = (orig, patch, result, path = '')=>{
     lodashEach(patch, (patchValue, patchPath)=>{
-        if(typeof patchValue === 'object') {
-            differ(orig, patchValue, result, `${path}${patchPath}.`)
-        } else if(['boolean','number', 'string'].includes(typeof patchValue)) {
+        if(['boolean','number', 'string'].includes(typeof patchValue)) {
             let a = lodashGet(orig, `${path}${patchPath}`)
             if(a !== patchValue){
                 lodashSet(result, `${path}${patchPath}`, patchValue)
             }
-        } 
+        } else if(typeof patchValue === 'object' && patchValue instanceof Date) {
+            let a = lodashGet(orig, `${path}${patchPath}`)
+            if(a.getTime() !== patchValue.getTime()){
+                lodashSet(result, `${path}${patchPath}`, patchValue)
+            }
+        } else if(typeof patchValue === 'object') {
+            differ(orig, patchValue, result, `${path}${patchPath}.`)
+        }
         //Ignore patchValue: function, symbol, undefined
     })
 }
